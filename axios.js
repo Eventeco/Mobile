145db9353b41b81214666c50f56693fa6e96dfa8
Aizaz Hassan <aizazhassan1999@gmail.com>
2022-02-21 19:57:00 +0300

    updated axios base url

--- axios.js
+++ axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import {Platform} from 'react-native';
 
 const instance = axios.create({
-  baseURL: 'http://10.0.2.2:8080',
+  baseURL:
+    Platform.OS === 'android'
+      ? 'http://10.0.2.2:8080'
+      : 'http://localhost:8080',
 });
 
 export default instance;
