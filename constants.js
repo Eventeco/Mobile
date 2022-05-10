1a0a8de3311a52782bf47f9fd45c179307295f8e
Aizaz Hassan <aizazhassan1999@gmail.com>
2022-05-11 02:08:36 +0300

    changed base url

--- constants.js
+++ constants.js
@@ -1,8 +1,7 @@
-import {Platform} from 'react-native';
+// import {Platform} from 'react-native';
 
-export const BASE_URL =
-  // 'https://eventeco.herokuapp.com';
-  Platform.OS === 'android' ? 'http://10.0.2.2:8080' : 'http://localhost:8080';
+export const BASE_URL = 'https://eventeco.herokuapp.com';
+// Platform.OS === 'android' ? 'http://10.0.2.2:8080' : 'http://localhost:8080';
 
 export const searchQueryParams = {
   name: '',
